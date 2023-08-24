63cfa579d00caf61093322252a4167a93eb50151
Jordan Schelew <jordan@websavers.ca>
2023-08-23 22:03:48 -0300

    v3.0
    - Code formatting consistency
    - Pricing display & currency fixes
    - Bug fixes
    - Add registration code

--- modules/addons/google_tag_manager/google_tag_manager.php
+++ modules/addons/google_tag_manager/google_tag_manager.php
@@ -58,7 +58,7 @@ function google_tag_manager_config(){
         'description' => 'Automatically includes the GTM embed codes and provides the necessary JavaScript dataLayer for eCommerce events',
         'author' => 'Websavers Inc.',
         'language' => 'english',
-        'version' => '2.0',
+        'version' => '3.0',
         'fields' => [
             'gtm-container-id' => [
                 'FriendlyName' => 'GTM Container ID',
